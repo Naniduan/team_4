6fd2def53c9fb1b52552449cbc1540d72264d77a
Naniduan <41004075+Naniduan@users.noreply.github.com>
2021-03-21 13:34:27 +0300

    Rewritten project

--- project_team_4.kt
+++ project_team_4.kt
@@ -1,42 +1,44 @@
-class Node(value: Long, exist: Boolean){
-    public var value: Long
-    public var exist: Boolean
-
-    init {
-        this.value = value
-        this.exist = exist
-    }
-
-    constructor(value: Long){
-        this.value = value
-        this.exist = true
-    }
+open class BSNode<T,S> (){
+    open lateinit val key: T
+    open val value: S
+    open var leftChild: BSNode
+    open var rightChild: BSNode
 }
 
-open class BinaryTree(root: Node){ // open означает что этот класс можно наследовать
-    private var nodes: Array<Node>
-
-    init {
-        this.nodes = Array(1, {root})
-    }
-
-    public fun leftChild(i: Int): Node{
-        return nodes[(i+1)*2-1]
-    }
-
-    public fun rightChild(i: Int): Node{
-        return nodes[(i+1)*2]
-    }
-
-    public fun add(value: Long){
-
-    }
-
-    public fun find(value: Long){
-
-    }
-
-    public fun delete(value: Long){
-
-    }
+enum class Color(){
+    RED,
+    BLACK
 }
+
+open class RBNode<T,S>: BSNode<T,S>(){
+    var color: Color
+    val parent: RBNode
+}
+
+open class AVLNode<T,S>: BSNode<T,S>(){
+    var heght: Int
+}
+
+open class BSTree<T,S> (){
+    open fun find(){}
+    open fun add(){}
+    open fun delete(){}
+    open fun iterate(){}
+}
+
+open class RBTree<T,S>: BSTree<T,S>(){
+    override fun find(){}
+    override fun add(){}
+    override fun delete(){}
+    override fun iterate(){}
+    fun balance(){}
+}
+
+open class AVLTree<T,S>: BSTree<T,S>(){
+    override fun find(){}
+    override fun add(){}
+    override fun delete(){}
+    override fun iterate(){}
+    fun balance(){}
+}
+
